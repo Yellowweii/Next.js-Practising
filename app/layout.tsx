ec5f4ea1fa99e69d8cb36273555762e26d47d487
yellowwei <1119765689@qq.com>
2023-10-08 20:35:35 +0800

    some function of the navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="container">
         <Navbar />
         {children}
         <Footer />
