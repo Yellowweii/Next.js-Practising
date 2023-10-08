3d7e25bda297540e73427628a9d44b56ebe28649
yellowwei <1119765689@qq.com>
2023-10-08 21:43:59 +0800

    finish the hero module

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div>
+        <Image src="/images/nextjs.png" alt="Next.js logo" width={200} height={160} />
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { icons } from "@/constant";
 
 const Footer = () => {
   return (
-    <div>
-        <div>&copy;2023 Lamamia. All rights reserved.</div>
+    <div className="flex text-sm justify-between items-center h-[50px]">
+      <p>&copy;2023 Lamamia. All rights reserved.</p>
+      <div className="flex items-center gap-[10px]">
+        {icons.map((icon) => (
+          <Image className="opacity-60 cursor-pointer" key={icon.id} src={icon.url} alt={icon.alt} width={15} height={15} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
