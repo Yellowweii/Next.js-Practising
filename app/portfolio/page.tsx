8915e029578d5a055c684656e5bdf0c5dd6f6ab1
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-10-09 12:58:24 +0800

    complete some function of the portfolio module (#8)
    * create some dynamic routers
    
    * init navbar and footer module
    
    * some function of the navbar
    
    * finish the hero module
    
    * update the about module
    
    * finish the about page
    
    * finish the contact page
    
    ---------
    
    Co-authored-by: yellowwei <1119765689@qq.com>

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { portfolioLinks } from "@/constant";
+import Link from "next/link";
+import React from "react";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <div>
+      <h1 className="text-4xl font-bold">Choose a gallery</h1>
+      <div className="flex gap-[50px]">
+        {portfolioLinks.map((link) => (
+          <Link key={link.id} className="border-[5px] border-[#bbb] rounded-[5px] w-[300px] h-[400px] relative" href={link.url}>
+            <span className="absolute right-[10px] bottom-[10px] text-[40px] font-bold">{link.text}</span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
