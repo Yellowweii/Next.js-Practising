2bae46338aa4956da880ab7c60c63de112ba76ce
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-10-09 18:14:24 +0800

    finish the portfolioDetail module (#10)
    * create some dynamic routers
    
    * init navbar and footer module
    
    * some function of the navbar
    
    * finish the hero module
    
    * update the about module
    
    * finish the about page
    
    * finish the contact page
    
    * complete some function of portfolio module
    
    * finish the portfolioDetail module
    
    ---------
    
    Co-authored-by: yellowwei <1119765689@qq.com>

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,14 +1,16 @@
 import { portfolioLinks } from "@/constant";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Portfolio = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Choose a gallery</h1>
-      <div className="flex gap-[50px]">
+      <h1 className="text-4xl font-bold my-5">Choose a gallery</h1>
+      <div className="flex gap-[50px] mb-[60px]">
         {portfolioLinks.map((link) => (
-          <Link key={link.id} className={`border-[5px] border-[#bbb] rounded-[5px] w-[300px] h-[400px] relative ${link.bg_img} bg-cover hover:text-[#53c28b]`} href={link.url}>
+          <Link key={link.id} className={`border-[5px] border-[#bbb] rounded-[5px] w-[300px] h-[400px] relative hover:text-[#53c28b]`} href={link.url}>
+            <Image className="object-cover" src={link.imgurl} alt={link.text} fill />
             <span className="absolute right-[10px] bottom-[10px] text-[40px] font-bold">{link.text}</span>
           </Link>
         ))}
