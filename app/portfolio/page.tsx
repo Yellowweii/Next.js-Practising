ad6e6257ff6fda41702b58b9a329918de41c9d32
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-10-09 16:43:37 +0800

    complete some function of portfolio module (#9)
    * create some dynamic routers
    
    * init navbar and footer module
    
    * some function of the navbar
    
    * finish the hero module
    
    * update the about module
    
    * finish the about page
    
    * finish the contact page
    
    * complete some function of portfolio module
    
    ---------
    
    Co-authored-by: yellowwei <1119765689@qq.com>

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -8,7 +8,7 @@ const Portfolio = () => {
       <h1 className="text-4xl font-bold">Choose a gallery</h1>
       <div className="flex gap-[50px]">
         {portfolioLinks.map((link) => (
-          <Link key={link.id} className="border-[5px] border-[#bbb] rounded-[5px] w-[300px] h-[400px] relative" href={link.url}>
+          <Link key={link.id} className={`border-[5px] border-[#bbb] rounded-[5px] w-[300px] h-[400px] relative ${link.bg_img} bg-cover hover:text-[#53c28b]`} href={link.url}>
             <span className="absolute right-[10px] bottom-[10px] text-[40px] font-bold">{link.text}</span>
           </Link>
         ))}
